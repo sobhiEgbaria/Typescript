2d36ad805185b1b7eb154dbd54392741d2c3e870
sobhiEgbaria <sobhim686@gmail.com>
2025-07-07 17:03:50 +0300

    map location points

--- location_search/src/App.tsx
+++ location_search/src/App.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import LocationSearch from "./components/LocationSearch";
 import Map from "./components/Map";
 import type { Place } from "./api/place";
 
 function App() {
+  const [place, setPlace] = useState<Place | null>(null);
   return (
     <div className="h-screen w-screen grid grid-cols-12">
       <div className="col-span-3 p-2">
-        <LocationSearch />
+        <LocationSearch onPlaceClick={(p) => setPlace(p)} />
       </div>
       <div className="col-span-9">
-        <Map />
+        <Map place={place} />
       </div>
     </div>
   );
--- location_search/src/components/LocationSearch.tsx
+++ location_search/src/components/LocationSearch.tsx
@@ -1,8 +1,53 @@
 import type { Place } from "../api/place";
-const LocationSearch = () => {
+import { Fragment, useState } from "react";
+import { search } from "../api/search";
+
+interface locationSearchProp {
+  onPlaceClick: (p: Place) => void;
+}
+const LocationSearch = ({ onPlaceClick }: locationSearchProp) => {
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handelSubmit = async (e: React.FocusEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setPlaces(await search(searchTerm));
+  };
+
+  const content = places.map((place) => {
+    return (
+      <Fragment key={place.id}>
+        <p className="text-sm">{place.name}</p>
+        <button
+          className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+          onClick={() => onPlaceClick(place)}
+        >
+          Go
+        </button>
+        <div className="border-b w-full col-span-2" />
+      </Fragment>
+    );
+  });
+
   return (
     <>
-      <h1>LocationSearch</h1>
+      <div>
+        <form action="" onSubmit={handelSubmit}>
+          <label className="font-bold" htmlFor="term">
+            Search
+          </label>
+          <input
+            id="term"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          />
+        </form>
+        <h1 className="font-bold mt-6">Found Locations</h1>
+        <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+          {content}
+        </div>
+      </div>
     </>
   );
 };
